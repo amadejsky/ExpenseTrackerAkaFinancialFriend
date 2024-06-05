c12add946af6a896724594a085dc524c949186df
amadejsky <adrian.madejski@vp.pl>
2024-06-05 20:42:14 +0200

    Controller approach routing added with blank .html templates prepared for Thymeleaf usage, three main urls / /app /about

--- project/src/main/java/com/madejsky/project/Controllers/MainEntityController.java
+++ project/src/main/java/com/madejsky/project/Controllers/MainEntityController.java
@@ -3,6 +3,7 @@ package com.madejsky.project.Controllers;
 import com.madejsky.project.Services.EntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +13,17 @@ public class MainEntityController {
     @Autowired
     EntityService service;
 
-
+    @RequestMapping(path="/")
+        public String homePage(Model model){
+        return "Home";
+    }
+    @RequestMapping(path="/app")
+        public String appPage(Model model){
+            return "App";
+    }
+    @RequestMapping(path="/about")
+        public String aboutPage(Model model){
+            return "About";
+    }
 
 }
