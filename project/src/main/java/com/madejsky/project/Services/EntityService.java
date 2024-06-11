89ebfc8665246cfb53a3fc5424806bcc0881aebe
amadejsky <adrian.madejski@vp.pl>
2024-06-11 17:17:05 +0200

    New service method to get all expenses, slightly new design

--- project/src/main/java/com/madejsky/project/Services/EntityService.java
+++ project/src/main/java/com/madejsky/project/Services/EntityService.java
@@ -6,6 +6,9 @@ import com.madejsky.project.Repository.EntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EntityService {
 
@@ -14,5 +17,13 @@ public class EntityService {
 
     public Expense addExpense(Expense expense){
         return repository.save(expense);
+    }
+
+    public List<Expense> getAll(){
+        List<Expense> result = (List<Expense>) repository.findAll();
+        if(result.size()>0)
+        return result;
+        else
+            return new ArrayList<Expense>();
     }
 }
